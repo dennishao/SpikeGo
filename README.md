050d2ce93da430e5a641703cf0c996f5eae1ebd3
dennis <623091113@qq.com>
2022-03-20 13:14:51 +0800

    feat: 新增gorm支持

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # SpikeGo
+
+
+###v1.0.0
+-----引入gin
+
+
+###v1.0.1
+-----引入热部署  air  https://github.com/darjun/go-daily-lib/tree/master/air
+
+-----引入gorm
+
+-----输出产品list
--- main.go
+++ main.go
@@ -3,11 +3,44 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"net/http"
 )
 
+type Product struct {
+	Id    int
+	Name  string
+	Price int
+}
+
+var DB *gorm.DB
+
+func init() {
+	//连接数据库
+	dsn := "root:root@tcp(127.0.0.1:3306)/ming?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("connect db err", err)
+	}
+
+	//插入模型
+	err1 := db.AutoMigrate(&Product{})
+	if err1 != nil {
+		fmt.Println("AutoMigrate err", err1)
+	}
+
+	DB = db
+}
+
 func SpikeController(c *gin.Context) {
 	c.String(http.StatusOK, "spike controller ok")
+}
+
+func getProducts(c *gin.Context) {
+	var products []Product
+	DB.Find(&products)
+	c.JSON(http.StatusOK, gin.H{"data": products})
 }
 
 func main() {
@@ -15,9 +48,12 @@ func main() {
 
 	g.GET("spike", SpikeController)
 
+	g.GET("/products", getProducts)
+
 	err := g.Run()
 
 	if err != nil {
 		fmt.Println("gin run err", err)
 	}
+
 }
