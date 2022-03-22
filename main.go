13249dba6fbca89f50d4c16dd7393f16acaab73f
dennis <623091113@qq.com>
2022-03-22 21:23:03 +0800

    feat: 增加抢购接口

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ type Product struct {
 	Id    int
 	Name  string
 	Price int
+	Sku   int
 }
 
 var DB *gorm.DB
@@ -33,22 +34,47 @@ func init() {
 	DB = db
 }
 
-func SpikeController(c *gin.Context) {
-	c.String(http.StatusOK, "spike controller ok")
-}
-
 func getProducts(c *gin.Context) {
 	var products []Product
 	DB.Find(&products)
+
 	c.JSON(http.StatusOK, gin.H{"data": products})
+}
+
+func addProduct(c *gin.Context) {
+	product := Product{Name: "lemon", Price: 8, Sku: 20000}
+	res := DB.Create(&product)
+
+	if res.RowsAffected == 1 {
+		c.JSON(http.StatusOK, gin.H{"code": 2000, "message": "新增成功 "})
+	}
+}
+
+func buyProduct(c *gin.Context) {
+	name := c.Param("name")
+	var product Product
+	res := DB.Find(&product, "name = ?", name)
+	if res.RowsAffected == 0 {
+		c.JSON(http.StatusOK, gin.H{"code": 2000, "message": "不存在此商品"})
+		return
+	}
+
+	product.Sku = product.Sku - 1
+	res1 := DB.Save(&product)
+	if res1.RowsAffected == 1 {
+		c.JSON(http.StatusOK, gin.H{"code": 2000, "message": "抢购成功"})
+		return
+	}
 }
 
 func main() {
 	g := gin.Default()
 
-	g.GET("spike", SpikeController)
-
 	g.GET("/products", getProducts)
+
+	g.POST("/products", addProduct)
+
+	g.GET("/kill/:name", buyProduct)
 
 	err := g.Run()
 
